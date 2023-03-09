61f01aaf050bb2be9b47f5781a0fc6aede385b65
Emma Larson <emmalarson@Emmas-MacBook-Pro.local>
2023-03-08 22:16:30 -0500

    Added the decode function

--- lab6.py
+++ lab6.py
@@ -1,3 +1,4 @@
+
 # Nicholas DeSanctis
 
 def encode(password):
@@ -8,6 +9,32 @@ def encode(password):
         else:
             encoded += str((int(i) + 3) % 10)
     return encoded
+
+def decoder(data):
+    password=''
+    for i in data:
+        if int (i)==0:
+            password+='7'
+        if int(i)== 1:
+            password+='8'
+        if int(i)==2:
+            password+='9'
+        elif int(i)==3:
+            password+='0'
+        elif int(i)==4:
+            password+='1'
+        elif int(i)==5:
+            password+='2'
+        elif int(i)==6:
+            password+='3'
+        elif int(i)==7:
+            password+='4'
+        elif int(i)==8:
+            password+='5'
+        elif int(i)==9:
+            password+='6'
+    return password
+
 
 
 if __name__ == '__main__':
@@ -25,7 +52,9 @@ if __name__ == '__main__':
             print("Your password has been encoded and stored!")
 
         elif user_opt == 2:
-            pass
+            print(f"The encoded password is {encoded_password}, and the original password is {decoder(encoded_password)}.")
+            print(decoder(encoded_password))
 
         elif user_opt == 3:
             loop_stop = True
+
